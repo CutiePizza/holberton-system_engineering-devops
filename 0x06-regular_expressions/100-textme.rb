5c4028a366c592126111d255c7788132735b17b5
CutiePizza <ines.chokri97@gmail.com>
2020-02-24 18:50:40 +0100

    task done

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-regex = /(from:)([A-Za-z0-9]+)(...)(to:)(\+?[A-Za-z0-9]+)(...)(flags:)((-?[0|1]:?)+)/
+regex = /(from:)(\+?[A-Za-z0-9]+)(...)(to:)(\+?[A-Za-z0-9]+)(...)(flags:)((-?[0|1]:?)+)/
 input = ARGV[0].scan(regex)
-print(input[0][1] + ", " + input[0][4] + ", " + input[0][7])
+print(input[0][1] + "," + input[0][4] + "," + input[0][7])
 print("\n")
