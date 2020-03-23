2d2c1b6291f2ffd7b5dcad0f494f43823137874c
CutiePizza <ines.chokri97@gmail.com>
2020-03-23 16:23:25 +0100

    fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create a file
 file {'/tmp/holberton':
+path => 'tmp/holberton',
 ensure => 'present',
 mode => '0744',
 owner => 'www-data',
 group => 'www-data',
-path => '/tmp/holberton',
 content => 'I love Puppet',
 }
