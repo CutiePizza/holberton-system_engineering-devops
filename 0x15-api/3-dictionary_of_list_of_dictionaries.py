9557c6e2cf47c8ddfa34c78b5e8ad12c4cc52cbf
CutiePizza <ines.chokri97@gmail.com>
2020-05-11 05:35:29 +0100

    fix

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -14,19 +14,19 @@ if __name__ == "__main__":
         i = 1
         my_employees = {}
         for user in r.json():
-            user_id = str(r.json()[i]['id'])
-            username = str(r.json()[i]['username'])
+            user_id = r.json()[i]['id']
+            username = r.json()[i]['username']
             r2 = requests.get(
                     'https://jsonplaceholder.typicode.com/users/{}/todos'
                     .format(
                         user_id)
                     )
-            my_employees[user_id] = []
+            my_employees[str(user_id)] = []
             for task in r2.json():
                 dicti = {}
                 dicti["username"] = username
                 dicti["task"] = task["title"]
                 dicti["completed"] = task["completed"]
-                my_employees[user_id].append(dicti)
+                my_employees[str(user_id)].append(dicti)
             i = i + 1
         json.dump(my_employees, json_file)
