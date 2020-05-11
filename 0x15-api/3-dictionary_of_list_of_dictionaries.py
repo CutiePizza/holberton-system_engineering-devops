9e7ee6d7779b58b1a039bcfd4289aec00cbbffe2
CutiePizza <ines.chokri97@gmail.com>
2020-05-11 05:15:46 +0100

    task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Task 3
+"""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    file_name = 'todo_all_employees.json'
+    with open(file_name, "w", newline="") as json_file:
+        r = requests.get('https://jsonplaceholder.typicode.com/users/')
+        i = 0
+        my_employees = {}
+        for user in r.json():
+            user_id = r.json()[i]['id']
+            username = r.json()[i]['username']
+            r2 = requests.get(
+                    'https://jsonplaceholder.typicode.com/users/{}/todos'
+                    .format(
+                        user_id)
+                    )
+            my_employees[user_id] = []
+            for task in r2.json():
+                dicti = {}
+                dicti["username"] = username
+                dicti["task"] = task["title"]
+                dicti["completed"] = task["completed"]
+                my_employees[user_id].append(dicti)
+            i = i + 1
+        json.dump(my_employees, json_file)
