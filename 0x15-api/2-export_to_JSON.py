6cc63e9e83fa8006792f50a0f0b9d988a5709c7f
CutiePizza <ines.chokri97@gmail.com>
2020-05-11 04:01:20 +0100

    task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Task 2
+"""
+import json
+import io
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    user_id = sys.argv[1]
+    r = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(
+        user_id)
+        )
+    username = r.json()['username']
+    r = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+                user_id)
+            )
+    my_tasks = {"2": []}
+    file_name = user_id + '.json'
+    with open(file_name, "w") as json_file:
+        for task in r.json():
+            dic = {}
+            dic["task"] = task["title"]
+            dic["completed"] = task["completed"]
+            dic["username"] = username
+            my_tasks["2"].append(dic)
+            dic = {}
+        json.dump(my_tasks, json_file)
