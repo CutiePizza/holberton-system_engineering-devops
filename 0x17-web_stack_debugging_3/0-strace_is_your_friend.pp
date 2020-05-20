a6fbb2d317d0546d02d7cac0630bdc70c604af8b
CutiePizza <ines.chokri97@gmail.com>
2020-05-20 19:02:31 +0100

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 exec {'Additional p removed':
-command  => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
 path     => '/usr/bin/:/bin/:/usr/sbin/',
 provider => 'shell',
+command  => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
 }
