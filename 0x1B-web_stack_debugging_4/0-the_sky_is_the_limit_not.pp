a6b84748606932f5ab4788dc6c6a37352a306862
CutiePizza <ines.chokri97@gmail.com>
2020-05-28 19:38:32 +0100

    restart nginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,13 @@
-# Removing extra p
-exec {'Additional p removed':
+# Changing value
+exec {'Changing value':
 path     => '/usr/bin/:/bin/:/usr/sbin/',
 provider => 'shell',
 command  => "sed -i 's/15/4096/' /etc/default/nginx",
+}
+
+# Restarting nginx
+exec {'Restart nginx':
+path     => '/usr/bin/:/bin/:/usr/sbin/',
+provider => 'shell',
+command  => "sudo service nginx restart",
 }
