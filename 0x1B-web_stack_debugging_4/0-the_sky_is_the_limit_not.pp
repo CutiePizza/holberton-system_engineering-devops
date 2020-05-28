b1abff4495d208fea8c3f16ea27218a548fb255c
CutiePizza <ines.chokri97@gmail.com>
2020-05-28 19:46:29 +0100

    fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -5,7 +5,6 @@ provider => 'shell',
 command  => "sudo sed -i 's/15/4096/g' /etc/default/nginx",
 }
 
-# Restarting nginx
 exec {'Restart nginx':
 path     => '/usr/bin/:/bin/:/usr/sbin/',
 provider => 'shell',
