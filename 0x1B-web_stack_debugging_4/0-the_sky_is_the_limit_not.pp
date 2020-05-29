b360de4d038390267cff80b7e54debd7fefcab66
CutiePizza <ines.chokri97@gmail.com>
2020-05-29 09:36:36 +0100

    put in same line

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,11 +2,5 @@
 exec {'Changing value':
 path     => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 provider => 'shell',
-command  => "sudo sed -i 's/15/5000/g' /etc/default/nginx",
-}
-
-exec {'Restart nginx':
-path     => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-provider => 'shell',
-command  => 'sudo service nginx restart',
+command  => "sudo sed -i 's/15/5000/g' /etc/default/nginx; sudo service nginx restrt",
 }
