cb183f6de462d6bee40e59979a070e9898864004
CutiePizza <ines.chokri97@gmail.com>
2020-05-29 09:34:58 +0100

    change path

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # Changing value
 exec {'Changing value':
-path     => '/usr/bin/:/bin/:/usr/sbin/',
+path     => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 provider => 'shell',
 command  => "sudo sed -i 's/15/5000/g' /etc/default/nginx",
 }
 
 exec {'Restart nginx':
-path     => '/usr/bin/:/bin/:/usr/sbin/',
+path     => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 provider => 'shell',
 command  => 'sudo service nginx restart',
 }
