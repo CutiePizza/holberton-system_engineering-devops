2393b5f64ed3fb8fe3cf89dce2e7054f93d109e7
CutiePizza <ines.chokri97@gmail.com>
2020-05-29 09:39:35 +0100

    fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Changing value
 exec {'Changing value':
-path     => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-command  => "sed -i 's/15/5000/g' /etc/default/nginx; sudo service nginx restart",
+path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+command => "sed -i 's/15/5000/g' /etc/default/nginx; sudo service nginx restart",
 }
